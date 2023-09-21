d87f7da3cda0b31433c998803b101731788a45e5
hassaammgl <h455a4mmehtab@gmail.com>
2023-09-22 03:25:42 +0500

    registeration functionality completed

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,69 @@
 import { User } from "../models/User.js";
+import Joi from "joi";
+import { bcryptPass, bcryptAnswer } from "../utils/Bcrypt.js";
 
 export const UserControllers = {
   registerUser: async (req, res) => {
-    console.log("Register");
+    try {
+      // password regex
+      var passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+      console.log("register called");
+
+      // user schema for validation purposes
+      const userObjSchema = Joi.object({
+        name: Joi.string().min(4).max(30).required(),
+        email: Joi.string().email(),
+        password: Joi.string().min(4).max(30).pattern(passwordRegex).required(),
+        confirmPassword: Joi.ref("password"),
+      });
+
+      const { error } = userObjSchema.validate(req.body);
+      // in case of error return error
+      if (error) {
+        console.log(error.message);
+        if (error.message === `"confirmPassword" must be [ref:password]`) {
+          return res.status(500).json({
+            success: false,
+            error: "Confirm Password is not matched with Password",
+          });
+        }
+        return res.status(500).json({
+          success: false,
+          error,
+        });
+      }
+      // getting user input from client
+      const { name, email, password } = req.body;
+      // check if user is already exist
+      const isExist = await User.findOne({ email: email });
+      if (isExist) {
+        return res.status(500).json({
+          success: false,
+          error: "User already exists",
+        });
+      }
+      // hashing password
+      const hashedPassword = await bcryptPass.hash(password);
+      // create new user
+      const newUser = new User({
+        name,
+        email,
+        password: hashedPassword,
+      });
+      // save new user to database
+      await newUser.save();
+      // return response
+      return res.status(201).json({
+        success: true,
+        message: "User registered successfully",
+      });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({
+        success: false,
+        error: error.message,
+      });
+    }
   },
   loginUser: async (req, res) => {},
   forgetpassword: async (req, res) => {},
--- utils/Bcrypt.js
+++ utils/Bcrypt.js
@@ -1,12 +1,20 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
 const saltRounds = 10;
 
 export const bcryptPass = {
-    hash: async (password) => {
-        return await bcrypt.hash(password, saltRounds);
-    },
-    compareHash: async (password, dbPassword) => {
-        return await bcrypt.compare(password, dbPassword);
-    }
-}
+  hash: async (password) => {
+    return await bcrypt.hash(password, saltRounds);
+  },
+  compareHash: async (password, dbPassword) => {
+    return await bcrypt.compare(password, dbPassword);
+  },
+};
+export const bcryptAnswer = {
+  hash: async (ans) => {
+    return await bcrypt.hash(ans, saltRounds);
+  },
+  compareHash: async (ans, dbAns) => {
+    return await bcrypt.compare(ans, dbAns);
+  },
+};
